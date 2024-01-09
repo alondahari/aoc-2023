1156acd1fca85c80c691034c3d70a89db4eb28c8
Alon Dahari <alon@uptechstudio.com>
2024-01-09 11:09:13 +0000

    day 4 part 2

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::min;
+
 use lazy_static::lazy_static;
 use regex::Regex;
 
@@ -17,6 +19,7 @@ fn parse_numbers(s: &str) -> Vec<u32> {
 struct Card {
     winning_numbers: Vec<u32>,
     my_numbers: Vec<u32>,
+    copies: u32,
 }
 
 impl Card {
@@ -25,10 +28,11 @@ impl Card {
         Card {
             winning_numbers: parse_numbers(parts.0),
             my_numbers: parse_numbers(parts.1),
+            copies: 1,
         }
     }
 
-    fn points_total(self) -> u32 {
+    fn points_total(&self) -> u32 {
         self.my_numbers.iter().fold(0, |acc, num| {
             if self.winning_numbers.contains(num) {
                 if acc == 0 {
@@ -41,14 +45,41 @@ impl Card {
             }
         })
     }
+
+    fn sum_winning(&self) -> usize {
+        self.my_numbers
+            .iter()
+            .filter(|num| self.winning_numbers.contains(num))
+            .count()
+    }
+}
+
+fn add_cards(cards: &mut [Card]) -> u32 {
+    let card = cards.first().unwrap();
+    let copies = card.copies;
+    let sum = min(card.sum_winning(), cards.len());
+    if sum > 0 {
+        cards[1..sum + 1]
+            .iter_mut()
+            .for_each(|c| c.copies += copies);
+    };
+    copies
 }
 
 fn main() {
+    let mut cards: Vec<Card> = INPUT_FILE.lines().map(Card::new).collect();
+    let mut total_cards = 0;
+
     println!(
         "Total Points: {}",
-        INPUT_FILE
-            .lines()
-            .map(Card::new)
+        cards
+            .iter()
             .fold(0, |acc, card| { acc + card.points_total() })
     );
+
+    for i in 0..cards.len() {
+        total_cards += add_cards(&mut cards[i..]);
+    }
+
+    println!("Total scratchcards (part 2): {}", total_cards);
 }
